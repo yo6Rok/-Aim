50093a5d2618d5e095ce4f9d849d15aa343f5b97
yo6Rok <legoncorporation@gmail.com>
2025-03-31 11:42:06 +0300

    Create scripts.js

---
+++ assets/js/scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('applicationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Заявка отправлена!');
+    // Здесь можно отправить данные на сервер или сохранить их в базе данных.
+});
