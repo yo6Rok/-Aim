6f1e5b733a61cf207adbf66479baaedffd6c254d
yo6Rok <legoncorporation@gmail.com>
2025-03-31 12:40:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,57 +1,44 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const session = require('express-session');
 const bodyParser = require('body-parser');
-const passport = require('passport');
-
-const app = express();
-
-// Подключение к базе данных MongoDB
-mongoose.connect('mongodb://localhost/cs2_clan', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("MongoDB подключен"))
-    .catch(err => console.log(err));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(session({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: false
-}));
-
-// Модели для заявок и пользователей
 const Application = require('./models/application');
 const User = require('./models/user');
 
-// Страница главная (отправка заявок)
-app.post('/applications', async (req, res) => {
-    const { username, email, message } = req.body;
-    const newApplication = new Application({ username, email, message });
-    await newApplication.save();
+const app = express();
+
+// Подключение к базе данных
+mongoose.connect('mongodb://localhost/cs2_clan', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Маршруты
+app.post('/register', async (req, res) => {
+    const { username, password, email } = req.body;
+    const user = new User({ username, password, email });
+    await user.save();
     res.redirect('/');
 });
 
-// Регистрация пользователя
-app.post('/register', async (req, res) => {
-    const { username, password, email } = req.body;
-    const newUser = new User({ username, password, email });
-    await newUser.save();
-    res.redirect('/login');
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username, password });
+    if (user) {
+        res.redirect('/dashboard');
+    } else {
+        res.redirect('/login');
+    }
 });
 
-// Логин пользователя
-app.post('/login', passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login'
-}));
-
-// Просмотр всех заявок для администраторов
-app.get('/admin/applications', async (req, res) => {
-    const applications = await Application.find();
-    res.render('admin', { applications });
+app.post('/applications', async (req, res) => {
+    const { username, email, message } = req.body;
+    const application = new Application({ username, email, message });
+    await application.save();
+    res.redirect('/');
 });
 
-// Стартуем сервер
+// Запуск сервера
 app.listen(3000, () => {
-    console.log("Сервер работает на http://localhost:3000");
+    console.log('Сервер работает на http://localhost:3000');
 });
